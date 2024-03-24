0f1b8e9e5a6ee5e237a487719ea74240d057f644
amirylm <amirylm.dev@gmail.com>
2024-03-25 00:31:56 +0200

    use subcmds

--- src/main.rs
+++ src/main.rs
@@ -8,38 +8,44 @@ fn main() {
         .version("0.0.1")
         // .author("Amir Y <amirylm.dev@gmail.com>")
         .about("Base 64 en/decoding")
-        .arg(Arg::new("cmd")
-                .short('c')
-                .long("cmd")
-                .help("Whether to encode (e) or decode (d)"))
-        .arg(Arg::new("file")
-                .short('f')
-                .long("file")
-                .help("File containing data to encode or decode"))
-        .arg(Arg::new("input")
-                .short('i')
-                .long("input")
-                .help("Five less than your favorite number"))
-        .get_matches();
+        .subcommand(Command::new("encode")
+                .about("Encode data")
+                .alias("e")
+                .arg(Arg::new("file")
+                        .short('f')
+                        .long("file")
+                        .help("File containing data to encode"))
+                .arg(Arg::new("input")
+                        .short('i')
+                        .long("input")
+                        .help("Raw string input")))
+        .subcommand(Command::new("decode")
+                .about("Decode data")
+                .alias("d")
+                .arg(Arg::new("file")
+                        .short('f')
+                        .long("file")
+                        .help("File containing data to decode"))
+                .arg(Arg::new("input")
+                        .short('i')
+                        .long("input")
+                        .help("Raw string input")))
+            .get_matches();
 
-    let d = String::from("");
-    let file_path = matches.get_one::<String>("file").unwrap_or(&d);
-    let file_contents;
-    let data: &str = if file_path.is_empty() {
-        matches.get_one::<String>("input").unwrap()
-    } else { 
-        file_contents = fs::read_to_string(file_path).unwrap_or(String::from(""));
-        file_contents.as_str()
-    };
-
-    let cmd = matches.get_one::<String>("cmd").unwrap();
-
-    match cmd.as_str() {
-        "e" => encoding::encode(data),
-        "encode" => encoding::encode(data),
-        "d" => encoding::decode(data),
-        "decode" => encoding::decode(data),
-        _ => println!("Unknown command")
-    }
-    
+        matches.subcommand().map(|(cmd, matches)| {
+            let empty_str = String::from("");
+            let file_path = matches.get_one::<String>("file").unwrap_or(&empty_str);
+            let file_contents;
+            let data: &str = if file_path.is_empty() {
+                matches.get_one::<String>("input").unwrap_or(&empty_str)
+            } else { 
+                file_contents = fs::read_to_string(file_path).unwrap_or(empty_str);
+                file_contents.as_str()
+            };
+            match cmd {
+                "encode" => encoding::encode(data),
+                "decode" => encoding::decode(data),
+                _ => println!("Unknown command")
+            }
+        });
 }
